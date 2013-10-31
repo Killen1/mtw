7a352bce9152c1176760b8c76223b1764d49975a
Nathan Killen <nathan.killen@colorado.edu>
2013-10-30 23:35:07 -0600

    functions.php

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Default_Theme
+ */
+
+//Add borwser class so we can style according to browser
+function organizedthemes_browser_body_class($classes) {
+ 
+    global $is_gecko, $is_IE, $is_safari, $is_chrome;
+ 
+    if($is_gecko)      $classes[] = 'firefox';
+    elseif($is_safari) $classes[] = 'safari';
+    elseif($is_chrome) $classes[] = 'chrome';
+    elseif($is_IE)     $classes[] = 'ie';
+    else               $classes[] = 'unknown';
+ 
+    return $classes;
+ 
+}
+add_filter('body_class','organizedthemes_browser_body_class');
+
+?>
