42a96d7027b3bc07c568559a7bcd7a01be084957
Nathan Killen <Killen1@users.noreply.github.com>
2013-10-31 00:20:05 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,11 +9,26 @@ function organizedthemes_browser_body_class($classes) {
  
     global $is_gecko, $is_IE, $is_safari, $is_chrome;
  
-    if($is_gecko)      $classes[] = 'firefox';
-    elseif($is_safari) $classes[] = 'safari';
-    elseif($is_chrome) $classes[] = 'chrome';
-    elseif($is_IE)     $classes[] = 'ie';
-    else               $classes[] = 'unknown';
+    if($is_gecko)
+    {
+    	$classes[] = 'firefox';
+    }
+    elseif($is_safari)
+    {
+    	$classes[] = 'safari';
+    }
+    elseif($is_chrome) 
+    {
+    	$classes[] = 'chrome';
+    }
+    elseif($is_IE)
+    {  
+		$classes[] = 'ie';
+	}
+    else
+    {            
+    	$classes[] = 'unknown';
+    }
  
     return $classes;
  
