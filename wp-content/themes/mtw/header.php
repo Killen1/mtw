bf4e6746ca578371a40ccccad838b956c28daecd
aquest <alex@amantecoffee.com>
2013-12-08 13:55:38 -0700

    Header (w/o jquery-1.10.2.min.js 'if-check')

--- wp-content/themes/mtw/header.php
+++ wp-content/themes/mtw/header.php
@@ -19,7 +19,6 @@
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 <script src="<?php echo SITE_ROOT ?>js/slide-gallery.js"></script>
 <script src="<?php echo SITE_ROOT ?>js/soundcloud.js"></script>
-<?php if ( is_page('contact')): ?><script src="<?php echo SITE_ROOT ?>js/jquery-1.10.2.min.js"></script><? endif; ?>
 <?php if ( is_page('contact')): ?><script src="<?php echo SITE_ROOT ?>jquery.validate.pack.js"></script><? endif; ?>
 <?php if ( is_page('contact')): ?><script src="<?php echo SITE_ROOT ?>js/bootstrap-contact.js"></script><? endif; ?>
 <body <?php body_class(); ?>>
