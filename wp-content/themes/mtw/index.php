fc3306ad69670ecb4b8e22abaae3aab354818d21
nkillen <nathan.killen@colorado.edu>
2013-12-08 14:15:39 -0700

    commit to resolve conflict

--- wp-content/themes/mtw/index.php
+++ wp-content/themes/mtw/index.php
@@ -2,36 +2,35 @@
 <div class="bg-paper">
 	<div class="container margin-bottom-20">
 		<div class="row">
-   				<?php $count = 1 ?>
-				<?php $latest_news = new WP_Query('cat=2&showposts=1'); ?>
-				<?php if($latest_news->have_posts()) : ?>
-					<?php while($latest_news->have_posts()) : ?>
-						<?php $latest_news->the_post(); ?>
-						<div class="col-xs-12 col-sm-12 col-md-8 col-lg-6">
-							<div class="row">
-								<div class="col-sm-12 padding-0">
-									<h2>Latest News</h2>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-sm-5 col-md-5 col-lg-6 margin-bottom-10 padding-0">
-									<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php echo get_the_post_thumbnail( $post_id, array(260,260) ); ?></a>
-								</div>
-								<div class="col-sm-7 col-md-7 col-lg-6">
-									<p>
-										<?php 
-							        		$excerpt = get_the_excerpt('','',FALSE);
-							        		echo substr($excerpt, 0, 400);
-							        		if(strlen($excerpt) > 400) echo "...";
-							        	?>
-									</p>
-									<a href="<?php the_permalink() ?>">Read More</a>
-								</div>
+			<?php $latest_news = new WP_Query('cat=2&showposts=1'); ?>
+			<?php if($latest_news->have_posts()) : ?>
+				<?php while($latest_news->have_posts()) : ?>
+					<?php $latest_news->the_post(); ?>
+					<div class="col-xs-12 col-sm-12 col-md-8 col-lg-6">
+						<div class="row">
+							<div class="col-sm-12 padding-0">
+								<h2>Latest News</h2>
 							</div>
 						</div>
-					<?php endwhile;?>
-				<?php endif; ?>
-				<?php wp_reset_query(); ?>
+						<div class="row">
+							<div class="col-sm-5 col-md-5 col-lg-6 margin-bottom-10 padding-0">
+								<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php echo get_the_post_thumbnail( $post_id, array(260,260) ); ?></a>
+							</div>
+							<div class="col-sm-7 col-md-7 col-lg-6">
+								<p>
+									<?php 
+						        		$excerpt = get_the_excerpt('','',FALSE);
+						        		echo substr($excerpt, 0, 400);
+						        		if(strlen($excerpt) > 400) echo "...";
+						        	?>
+								</p>
+								<a href="<?php the_permalink() ?>">Read More</a>
+							</div>
+						</div>
+					</div>
+				<?php endwhile;?>
+			<?php endif; ?>
+			<?php wp_reset_query(); ?>
 			<!--Loop Goes here-->
 			<div class="col-xs-12 col-sm-12 col-md-4 col-lg-6">
 				<h2>Upcoming Shows</h2>
@@ -90,7 +89,6 @@
 							    });
 							}());
 							</script>
-			
 						</div>
 					</div>
 				</div>
