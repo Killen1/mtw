ce0ab79f6cf0fc6ebf7c9102e3b7cb7a268e9cf4
aquest <alex@amantecoffee.com>
2013-12-13 17:37:37 -0700

    Music page

--- wp-content/themes/mtw/music.php
+++ wp-content/themes/mtw/music.php
@@ -7,64 +7,28 @@
 //{debug}	
 // echo "<pre>"; print_r(); die();
 get_header(); ?>
-<div class="bg-paper">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12 col-md-12 col-lg-12">
-				<div>
-					<h2>Music</h2>
-				</div>
-				<div class="row">
-					<div class="col-sm-5 col-md-4 col-lg-4">
-						<a href="#" title=""><img src="http://placehold.it/250x250" alt=""></a>
-					</div>
-					<div class="col-sm-7 col-md-8 col-lg-8">
-						<h3>The Ides EP</h3>
-						<p>Brownie muffin chocolate gummi bears gummi bears liquorice. Donut marzipan jelly dessert tart icing pie danish pudding.Liquorice soufflé lollipop oat cake muffin. Tart chocolate danish halvah lollipop croissant wafer powder.</p>
-						<ol>
-							<li>Track 1 | 00:00</li>
-							<li>Track 2 | 00:00</li>
-							<li>Track 3 | 00:00</li>
-							<li>Track 4 | 00:00</li>
-						</ol>
-						<p><a href="#" title="">Buy Now</a></p>
-					</div>
-				</div>
-				<div class="margin-top-20 row">
-					<div class="col-sm-5 col-md-4 col-lg-4">
-						<a href="#" title=""><img src="http://placehold.it/250x250" alt=""></a>
-					</div>
-					<div class="col-sm-7 col-md-8 col-lg-8">
-						<h3>The Ides EP</h3>
-						<p>Brownie muffin chocolate gummi bears gummi bears liquorice. Donut marzipan jelly dessert tart icing pie danish pudding.Liquorice soufflé lollipop oat cake muffin. Tart chocolate danish halvah lollipop croissant wafer powder.</p>
-						<ol>
-							<li>Track 1 | 00:00</li>
-							<li>Track 2 | 00:00</li>
-							<li>Track 3 | 00:00</li>
-							<li>Track 4 | 00:00</li>
-						</ol>
-						<p><a href="#">Buy Now</a></p>
-					</div>
-				</div> 
-				<div class="margin-top-20 row">
-					<div class="col-sm-5 col-md-4 col-lg-4">
-						<a href="#"><img src="http://placehold.it/250x250" alt=""></a>
-					</div>
-					<div class="col-sm-7 col-md-8 col-lg-8">
-						<h3>The Ides EP</h3>
-						<p>Brownie muffin chocolate gummi bears gummi bears liquorice. Donut marzipan jelly dessert tart icing pie danish pudding.Liquorice soufflé lollipop oat cake muffin. Tart chocolate danish halvah lollipop croissant wafer powder.</p>
-						<ol>
-							<li>Track 1 | 00:00</li>
-							<li>Track 2 | 00:00</li>
-							<li>Track 3 | 00:00</li>
-							<li>Track 4 | 00:00</li>
-						</ol>
-						<p><a href="#" title="">Buy Now</a></p>
-					</div>
-				</div>
-			</div>	
+	<div class="row">
+		<div class="col-xs-12">
+			<h1 class="text-center">Music</h1>
 		</div>
 	</div>
+	<?php $music = new WP_Query('category_name=music'); ?>
+	<?php if($music->have_posts()) : ?>
+		<?php while($music->have_posts()): $music->the_post(); ?>
+			<div id="<?php the_ID(); ?>" class="music-post-wrapper margin-left-20 margin-top-20">
+				<div class="row">
+					<div class="col-sm-5 col-md-4 col-lg-4">
+						<?php echo get_the_post_thumbnail( $post_id, array(250,250), array('alt' => 'Music Image') ); ?>
+					</div>
+					<div class="col-sm-7 col-md-8 col-lg-8">
+						<h3><?php the_title(); ?></h3>
+						<p><?php the_content();?></p>
+					</div>
+				</div>
+            </div>
+		<?php endwhile;?>
+	<?php endif; ?>
+	<?php wp_reset_query(); ?>
 	<div class="margin-top-20 row">
 		<div class="col-sm-12 padding-0">
 			<div id="wrapper" class="bg-fabric">
@@ -96,7 +60,4 @@ get_header(); ?>
 			</div>
 		</div>
 	</div>
-</div>	
 <?php get_footer(); ?>
-
-						
